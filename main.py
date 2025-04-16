aa6dd93aac8e6374696acd2dad0d518c8f4fe846
zigmasvino <97bender123@gmail.com>
2025-04-16 16:19:17 +0300

    Added: player tritriangle, movement, sprite groups

--- main.py
+++ main.py
@@ -1,23 +1,54 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    # Initialize the game
     pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Set up the game clock
+    clock = pygame.time.Clock()
+    # Time since the last frame in seconds
+    dt = 0
+
+    # Setting up sprite groups - these are used to manage and update the game objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = updatable, drawable
+
+    # Add the player to the updatable and drawable groups
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    # Set up the game window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+
+    # Infinite loop to keep the game running
     while True:        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:                
                 return
-            
+        
+        # Fill the screen with black 
         screen.fill((0, 0, 0))
-        pygame.display.flip()
+        
+        # Update the game objects
+        updatable.update(dt)
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        for sprite in drawable:            
+            # Draw the game objects
+            sprite.draw(screen)      
+                   
+        
+        pygame.display.flip()
+        # dt is the time since the last frame in seconds
+        dt = clock.tick(60) / 1000.0
+        # Limit the frame rate to 60 FPS
+        clock.tick(60)
+
+    
 
     
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,45 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+class Player(CircleShape):
+    """
+    Player class that inherits from CircleShape.
+    """
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0        
+
+    # triangle method to get the vertices of the triangle representing the player
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        # Draw the player as a triangle
+        pygame.draw.polygon(screen, (255, 255, 255), self.triangle(),3)
+    
+    def rotate(self, dt):
+        # Rotate the player
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
